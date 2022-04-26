89f7a917df007b06c74988ff7a6cece0c6c76f36
Jacobo Tabernero Rey <jacoborus@gmail.com>
2022-04-26 02:15:43 +0200

    Update: query fields

--- src/lite-sdk.test.ts
+++ src/lite-sdk.test.ts
@@ -2,15 +2,24 @@ import { assertEquals } from "https://deno.land/std@0.136.0/testing/asserts.ts";
 
 import LiteSdk from "./lite-sdk.ts";
 
+const apiUrl = "https://example.com";
+const sdk = new LiteSdk(apiUrl);
+
 Deno.test("class has API_URL prop", () => {
-  const apiUrl = "https://example.com";
-  const sdk = new LiteSdk(apiUrl);
   assertEquals(sdk.apiUrl, apiUrl);
 });
 
 Deno.test("simple query path", () => {
-  const apiUrl = "https://example.com";
-  const sdk = new LiteSdk(apiUrl);
   const result = "https://example.com/my/path";
   assertEquals(sdk.query("my/path"), result);
+});
+
+Deno.test("query with fields as string", () => {
+  const result = "https://example.com/my/path?fields=uno,dos,tres.*";
+  assertEquals(
+    sdk.query("my/path", {
+      fields: "uno,dos,tres.*",
+    }),
+    result,
+  );
 });
--- src/lite-sdk.ts
+++ src/lite-sdk.ts
@@ -1,11 +1,30 @@
+interface QueryParams {
+  fields?: string | string[];
+}
+
+function getQueryParams(options?: QueryParams): string {
+  const opts = options || ({} as QueryParams);
+  function renderFields() {
+    const fields = opts.fields;
+    return !fields ? "" : `fields=${fields}`;
+  }
+  const query = [
+    renderFields(),
+  ]
+    .filter((x) => x)
+    .join("&");
+  return query ? "?" + query : "";
+}
+
 export class LiteSdk {
   apiUrl: string;
   constructor(apiUrl: string) {
     this.apiUrl = apiUrl;
   }
 
-  query(path: string) {
-    return `${this.apiUrl}/${path}`;
+  query(path: string, params?: QueryParams) {
+    const queryString = getQueryParams(params);
+    return `${this.apiUrl}/${path}${queryString}`;
   }
 }
 
